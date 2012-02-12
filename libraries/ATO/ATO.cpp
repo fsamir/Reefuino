0af381a3a57e2de284c7a9f4357c7c3d29f65716
Franklin Dattein <franklin@dattein.com>
2012-02-13 07:56:40 +1100

    Added schematics sample to the ATO library

--- libraries/ATO/ATO.cpp
+++ libraries/ATO/ATO.cpp
@@ -1,19 +1,25 @@
 #include "ATO.h"
 
 //Schematics: http://www.arduino.cc/en/Tutorial/Button
-ATO::ATO(int pin, ReefuinoRelay relay):atoPin(pin), pumpRelay(relay){
-	  pinMode(atoPin, INPUT);
-	  Serial.println("Startd ATO on pin: "+ atoPin);
+/*
+ (GND) --- (10k-Resistor) ----|---- (float switch) --- (+5v)
+                              |
+                           Analog Pin
+ */
+ATO::ATO(int pin, ReefuinoRelay relay) :
+		atoPin(pin), pumpRelay(relay) {
+	pinMode(atoPin, INPUT);
+	Serial.println("Startd ATO on pin: " + atoPin);
 }
 //<<destructor>>
-ATO::~ATO(){/*nothing to destruct*/
+ATO::~ATO() {/*nothing to destruct*/
 }
 
 bool ATO::onLoop() {
 	int value = digitalRead(atoPin);
-	if(value == HIGH) {
+	if (value == HIGH) {
 		pumpRelay.turnOn();
-	}else{
+	} else {
 		pumpRelay.turnOff();
 	}
 
@@ -23,6 +29,4 @@ bool ATO::onLoop() {
 bool ATO::isTopping() {
 	return pumpRelay.isOn();
 }
-
-
 
